30effda83cdcc47e00480c08bb28ae838afc367f
Siruyy <your-email@example.com>
2025-07-06 20:49:13 +0800

    Added dependency

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <SpeedInsights />
+    <Analytics />
     <TooltipProvider>
       <Toaster />
       <Sonner />
